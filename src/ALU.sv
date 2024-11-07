62744dc0910d7c0efda227571ba0b1328fc8d9e7
Mirailisc <mirailisclm@gmail.com>
2024-11-07 23:06:39 +0700

    use tenary

--- src/ALU.sv
+++ src/ALU.sv
@@ -1,21 +1,23 @@
 module ALU (
-    input [2:0] ALU_Oper,     // 3-bit operation selector
-    input [7:0] A, B,         // 8-bit input operands
-    output reg [7:0] ALU_Out, // 8-bit output
-    output reg CY             // Carry output
+    input [2:0] ALU_Oper,
+    input [7:0] A, B,
+    output [7:0] ALU_Out,
+    output CY
 );
 
-    always @(*) begin
-        case (ALU_Oper)
-            3'b000: {CY, ALU_Out} = {1'b0, A + B};                 // Add
-            3'b001: {CY, ALU_Out} = {1'b0, A + 8'd1};              // Increment
-            3'b010: {CY, ALU_Out} = {A < B, A - B};                // Subtract (CY is borrow)
-            3'b011: {CY, ALU_Out} = {1'b0, A - 8'd1};              // Decrement
-            3'b100: {CY, ALU_Out} = {A[7], A << 1};                // Shift left (CY is MSB of A)
-            3'b101: {CY, ALU_Out} = {A[0], A >> 1};                // Shift right (CY is LSB of A)
-            3'b110: {CY, ALU_Out} = {1'b0, ~(A & B)};              // NAND
-            3'b111: {CY, ALU_Out} = {1'b0, (A > B) ? 8'd1 : 8'd0}; // If Greater Than
-            default: {CY, ALU_Out} = {1'b0, 8'd0};                 // Default case: 0
-        endcase
-    end
+assign {CY, ALU_Out} = ALU_Oper[2] ? 
+                        (ALU_Oper[1] ? 
+                            (ALU_Oper[0] ? 
+                                ((A > B) ? ({1'b0, 8'b1}) : ({1'b0, 8'b0})) : // If A > B
+                                ({1'b0, ~(A & B)})) : // NAND
+                            (ALU_Oper[0] ? 
+                                ({1'b0, A >> 1}) :  // SHR
+                                ({1'b0, A << 1}))) :  // SHL
+                        (ALU_Oper[1] ? 
+                            (ALU_Oper[0] ? 
+                                ({1'b0, A} - 1) : // Decrement
+                                ({1'b0, A} - {1'b0, B})) :  // Subtract
+                            (ALU_Oper[0] ? 
+                                ({1'b0, A} + 1) : // Increment
+                                ({1'b0, A} + {1'b0, B}))); // Add
 endmodule
