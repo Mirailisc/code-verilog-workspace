19f16356f18448ef1b3eb348beb473c402a1f610
Mirailisc <mirailisclm@gmail.com>
2024-11-08 09:13:29 +0700

    aaaa

--- src/ALU.sv
+++ src/ALU.sv
@@ -11,8 +11,8 @@ assign {CY, ALU_Out} = ALU_Oper[2] ?
                                 ((A > B) ? ({1'b0, 8'b1}) : ({1'b0, 8'b0})) : // If A > B
                                 ({1'b0, ~(A & B)})) : // NAND
                             (ALU_Oper[0] ? 
-                                ({1'b0, A >> 1}) :  // SHR
-                                ({1'b0, A << 1}))) :  // SHL
+                                ({A[0], A >> 1}) :  // SHR
+                                ({A[7], A << 1}))) :  // SHL
                         (ALU_Oper[1] ? 
                             (ALU_Oper[0] ? 
                                 ({1'b0, A} - 1) : // Decrement
